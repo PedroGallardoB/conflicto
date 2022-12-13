2981384a2231b3e9e08fec1e934cdf25a9b82954
pgallardo <pgallardo@106-08.info2>
2022-12-13 09:46:25 +0100

    Intento de resolución

--- Conflictos/src/conflictos/Conflictos.java
+++ Conflictos/src/conflictos/Conflictos.java
@@ -4,6 +4,9 @@ public class Conflictos {
 
 	public static void main(String[] args) {
 
+		
+		System.out.println("Hola soy, Juanma Gallego");
+
 		System.out.println("Hola soy Pedro Gallardo");
 
 	}
